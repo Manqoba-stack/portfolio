22f5e263c9222647f6a884bb93ee3e8a7eb7353a
Manqoba Makhoba <Manyoba1997@gmail.com>
2024-01-27 23:31:02 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector(".nav");
+
+const panels=document.querySelectorAll(".panel");
+window.addEventListener("scroll", alignNav);
+
+function alignNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
