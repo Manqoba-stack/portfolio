9b63abe157ce2e0032dd1f6503fb96349d7234a7
Manqoba Makhoba <Manyoba1997@gmail.com>
2024-01-29 19:32:29 +0200

    attemping firebase db

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector(".nav");
 
-const panels=document.querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel");
 window.addEventListener("scroll", alignNav);
 
 function alignNav() {
@@ -21,3 +21,37 @@ function removeActiveClasses() {
     panel.classList.remove("active");
   });
 }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVqWjKc-MARejU5wu_tUUk9fM9xWLOQoQ",
+  authDomain: "portfolio-api-bb59c.firebaseapp.com",
+  databaseURL: "https://portfolio-api-bb59c-default-rtdb.firebaseio.com",
+  projectId: "portfolio-api-bb59c",
+  storageBucket: "portfolio-api-bb59c.appspot.com",
+  messagingSenderId: "711410063207",
+  appId: "1:711410063207:web:87103eff71e959443601c2",
+  measurementId: "G-CGYT8XL5V7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get form data
+    const name = document.getElementById("fname").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("subject").value;
+
+    var database = firebase.database();
+    database.ref("message").set({
+      name: name,
+      email: email,
+      message: message,
+    });
+  });
+});
